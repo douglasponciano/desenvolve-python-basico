6e6fddf893cdece7de5afde4cf85a1e910d918bf
Douglas Ponciano <silvadgp@yahoo.com.br>
2024-06-12 16:04:54 -0300

    modulo2

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,14 @@
+#1) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+#Armazene o valor 5 em uma variável e o valor 2 em outra variável
+#mprima o tipo de dados dessas duas variáveis
+#ivida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) e armazene o resultado em uma terceira variável
+#Imprima a terceira variável e também o seu tipo
+
+num1 =5
+num2 =2 
+
+divisao = num1/num2
+
+print(type(num1),type(num2))
+
+print(divisao,type(divisao))
