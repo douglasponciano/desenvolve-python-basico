a61efccff6c60ef49c6024df88f46b2285758d08
Douglas Ponciano <silvadgp@yahoo.com.br>
2024-06-12 17:33:01 -0300

    exercicios-Tipos de Dados

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,14 @@
+#2) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+#Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+#Some os valores da segunda e terceira variável e armazene em outra variável.
+#Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+
+num1 =10
+num2 =3.5
+
+
+soma = num1+num2
+texto = "o resultado é"
+
+print(num1,type(num1),num2,type(num2),texto,type(texto),soma,type(soma))
---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,10 @@
+#4) Uma conta poupança foi aberta com um depósito de R$500,00. Esta conta é remunerada em 1% de juros ao mês. O código a seguir apresenta uma forma de implementação para calcular três meses de acúmulo de juros. Reescreva esse código usando apenas duas variáveis. 
+
+juros = 3.03
+saldo = 500.0
+
+
+
+print("Após 3 meses meu novo saldo é")
+print(juros * saldo)
+
